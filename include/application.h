38cc503f03e27e716854e75bd84b4458f643e077
unknown <vooleksandrov22@codingburgas.bg>
2024-12-06 19:36:10 +0200

    Implement notify function of APPLICATION. Separate declaration and implementation. Close #9

--- include/application.h
+++ include/application.h
@@ -3,27 +3,26 @@
 // Header file, where are declared essential components for interacting with QApplication and QWidget(main window)
 //
 
+
 #ifndef APPLICATION_H
 #define APPLICATION_H
 
 
 #include <iostream>
+#include <vector>
+
 #include <QApplication>
 #include <QWidget>
-#include <QFile>
-#include <QUiLoader>
-#include <QString>
 
 #include "algorithms.h"
-#include "api.h"
-
+#include "arrayModificationInfo.h"
+#include "notifications.h"
 
 
 /*
  *Struct, which stores essential objects for application, for convenience
  */
-struct APPLICATION {
-
+struct APPLICATION{
     QApplication *app;
     QWidget *window;
     std::vector<int> arrayToBeSorted;
@@ -36,25 +35,7 @@ struct APPLICATION {
      * Returns:
      * --status code
      */
-    int run(){
-        window->show();
-
-        int code =  app->exec();
-
-        switch (code) {
-        case 0:
-            std::cout << "Application exited successfully! \n";
-            break;
-        default:
-            std::cout << "Unknown error occured. \n";
-            break;
-        }
-
-        return code;
-    }
-
-
-
+    int run();
 
     /*
      * Performs an algorithm on array specified by user
@@ -63,16 +44,7 @@ struct APPLICATION {
      * Returns:
      * --None
      */
-    void performAlgorithm(std::string &name){
-
-        if (name == "bubbleSort"){
-            auto changes = SORTING::bubbleSort(arrayToBeSorted);
-            updateArrayOnUI(changes);
-        } else{
-            std::cerr << "Unknown algorithm. \n";
-        }
-    }
-
+    void performAlgorithm(std::string &name);
 
 
     /*
@@ -82,15 +54,9 @@ struct APPLICATION {
      * Returns:
      * --None
      */
-    void updateArrayOnUI(std::vector< std::vector<arrayModificationInfo*> > &changes){
-        // DO STUFF
-        for (size_t i=0; i<changes.size(); i++){
-            for (size_t j=0; j<changes[i].size(); j++){
-                std::cout << "Element on " << changes[i][j]->elementIndex << " to " << changes[i][j]->movedTo << "\n";
-            }
-            std::cout << "---------------------------------\n";
-        }
-    }
+    void updateArrayOnUI(std::vector< std::vector<arrayModificationInfo*> > &changes);
+
+    void notify(std::string message, NotificationType type);
 };
 
 
@@ -102,27 +68,7 @@ struct APPLICATION {
  * Returns:
  * --Pointer to an APPLICATION
  */
-APPLICATION* init(int argc, char *argv[]){
-
-    auto *app = new QApplication(argc, argv);
-
-
-    //open file with ui
-    QFile file(":/main.ui");
-    file.open(QFile::ReadOnly);
-
-    QUiLoader loader;
-    QWidget *window = loader.load(&file);
-    file.close();
-
-
-    auto *application = new APPLICATION{
-        app,
-        window
-    };
-
-    return application;
-}
+APPLICATION* init(int argc, char *argv[]);
 
 
 
---
+++ include/notifications.h
@@ -0,0 +1,16 @@
+#ifndef NOTIFICATIONS_H
+#define NOTIFICATIONS_H
+
+/*
+ * Enumaration type, representing type of notification
+ */
+enum NotificationType{
+    SUCCESS,
+    ERROR,
+    UNKNOWN
+};
+
+
+
+
+#endif // NOTIFICATIONS_H
