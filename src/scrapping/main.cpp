0e19bd72ec3c32847a7d3537a5e9adf37c82a75b
Petar Kostov <PTKostov22@codingburgas.bg>
2025-01-03 22:26:41 +0200

    Expand vocabulary

--- src/scrapping/main.cpp
+++ src/scrapping/main.cpp
@@ -12,13 +12,32 @@ int main(int argc, char *argv[]){
     //createWordsFile("https://dictionary.cambridge.org/dictionary/english/game", 1500);
     //createWordsFile("https://dictionary.cambridge.org/dictionary/english/programming", 1000);
     //createWordsFile("https://dictionary.cambridge.org/dictionary/english/lives", 3);
-    createWordsFile("https://dictionary.cambridge.org/dictionary/english/master", 1501);
+    // createWordsFile("https://dictionary.cambridge.org/dictionary/english/master", 1501);
 
-    createWordsFile("https://dictionary.cambridge.org/dictionary/english/creation", 2000);
+    // createWordsFile("https://dictionary.cambridge.org/dictionary/english/creation", 3000);
 
-    createWordsFile("https://dictionary.cambridge.org/dictionary/english/history", 2500);
+    // createWordsFile("https://dictionary.cambridge.org/dictionary/english/history", 4000);
 
-    createWordsFile("https://dictionary.cambridge.org/dictionary/english/slave", 3000);
+    // createWordsFile("https://dictionary.cambridge.org/dictionary/english/slave", 5000);
 
+    // createWordsFile("https://dictionary.cambridge.org/dictionary/english/expression", 6000);
+
+    // createWordsFile("https://dictionary.cambridge.org/dictionary/english/determine", 7000);
+
+    // createWordsFile("https://dictionary.cambridge.org/dictionary/english/external", 8000);
+
+    // createWordsFile("https://dictionary.cambridge.org/dictionary/english/epidural", 9000);
+
+    // createWordsFile("https://dictionary.cambridge.org/dictionary/english/restriction", 10000);
+
+    // createWordsFile("https://dictionary.cambridge.org/dictionary/english/equivalent", 12000);
+
+    // createWordsFile("https://dictionary.cambridge.org/dictionary/english/recommendation", 14000);
+
+    // createWordsFile("https://dictionary.cambridge.org/dictionary/english/independence", 16000);
+
+    // createWordsFile("https://dictionary.cambridge.org/dictionary/english/home-rule", 18000);
+
+    createWordsFile("https://dictionary.cambridge.org/dictionary/english/advancement", 20000);
     std::cout << "End\n";
 }
